570f171a91f9afffce52279ba53badde29dfd8a7
charlieroth <charlieroth4@gmail.com>
2024-05-27 20:39:51 +0200

    init and echo decoding/reply complete

--- Package.swift
+++ Package.swift
@@ -5,6 +5,9 @@ import PackageDescription
 
 let package = Package(
     name: "FlyDistSwift",
+    platforms: [
+        .macOS(.v14)
+    ],
     targets: [
         .executableTarget(
             name: "FlyDistSwift",
--- Sources/FlyDistSwift.swift
+++ Sources/FlyDistSwift.swift
@@ -1,39 +1,124 @@
+// swift-tools-version:5.10
+
 import Foundation
 
-struct InitMessageBody: Decodable {
+class StandardError: TextOutputStream {
+  func write(_ string: String) {
+    try! FileHandle.standardError.write(contentsOf: Data(string.utf8))
+  }
+}
+
+struct InitBody: Decodable {
     var type: String
     var msg_id: Int
     var node_id: String
     var node_ids: [String]
 }
 
-struct MessageBody: Decodable {
+struct InitReply: Encodable {
     var type: String
-    var msg_id: String?
-    var in_reply_to: String?
+    var in_reply_to: Int
+    var msg_id: Int
+    
+    init(type: String, in_reply_to: Int) {
+        self.type = type
+        self.in_reply_to = in_reply_to
+        self.msg_id = Int(arc4random())
+    }
 }
 
-struct Message<T: Decodable>: Decodable {
+struct EchoBody: Decodable {
+    var type: String
+    var msg_id: Int
+    var echo: String
+}
+
+struct EchoReply: Encodable {
+    var type: String
+    var in_reply_to: Int
+    var echo: String
+    var msg_id: Int
+    
+    init(type: String, in_reply_to: Int, echo: String) {
+        self.type = type
+        self.in_reply_to = in_reply_to
+        self.echo = echo
+        self.msg_id = Int(arc4random())
+    }
+}
+
+enum MessageBody {
+    case `init`(InitBody)
+    case echo(EchoBody)
+}
+
+extension MessageBody: Decodable {
+    enum CodingKeys: CodingKey {
+        case `init`, echo
+    }
+    
+    init(from decoder: Decoder) throws {
+        if let body = try? InitBody(from: decoder) {
+            self = .`init`(body)
+        } else if let body = try? EchoBody(from: decoder) {
+            self = .echo(body)
+        } else {
+            fatalError()
+        }
+    }
+}
+
+struct Message: Decodable {
+    var src: String
+    var dest: String
+    var body: MessageBody
+}
+
+struct Reply<T: Encodable>: Encodable {
     var src: String
     var dest: String
     var body: T
 }
 
 actor Node {
-    var id: Int
-    var nodes: [String]
+    var id: String? = nil
+    var nodes: [String]? = nil
     
-    init(id: Int, nodes: [String]) {
-        self.id = id
-        self.nodes = nodes
+    func handleInit(message: Message, body: InitBody) -> Reply<InitReply> {
+        self.id = body.node_id
+        self.nodes = body.node_ids
+        
+        return Reply<InitReply>(
+            src: body.node_id,
+            dest: message.src,
+            body: InitReply(
+                type: "init_ok",
+                in_reply_to: body.msg_id
+            )
+        )
+    }
+    
+    func handleEcho(message: Message, body: EchoBody) -> Reply<EchoReply> {
+        return Reply<EchoReply>(
+            src: self.id!,
+            dest: message.src,
+            body: EchoReply(
+                type: "echo_ok",
+                in_reply_to: body.msg_id,
+                echo: body.echo
+            )
+        )
     }
 }
 
 
 @main
 struct FlyDistSwift {
-    static func main() {
-        // let input = readLine(strippingNewline: true)!
+    static func main() async throws {
+        // while let input = readLine(strippingNewline: true) {
+        // }
+        let node = Node()
+        let jsonEncoder = JSONEncoder()
         let initMessage = """
         {
           "src": "n1",
@@ -46,23 +131,42 @@ struct FlyDistSwift {
           }
         }
         """
-        
-        // Parsing JSON into a dictionary
-        guard let data = initMessage.data(using: .utf8),
-              let parsedJSON = try? JSONSerialization.jsonObject(with: data, options: []) as? [String:[String:Any]] else {
-            fatalError("Failed to parse JSON")
+        let echoMessage = """
+        {
+          "src": "n1",
+          "dest": "n3",
+          "body": {
+              "type": "echo",
+              "msg_id": 2,
+              "echo": "Please echo 35"
+          }
+        }
+        """
+        let messages = [initMessage, echoMessage]
+        for message in messages {
+            print("incoming message:\n\(message)")
+            let messageData = message.data(using: .utf8)!
+            let decodedMessage = try! JSONDecoder().decode(Message.self, from: messageData)
+            switch decodedMessage.body {
+            case .`init`(let body):
+                let reply = await node.handleInit(
+                    message: decodedMessage,
+                    body: body
+                )
+                let jsonReply = try jsonEncoder.encode(reply)
+                let jsonReplyString = String(data: jsonReply, encoding: .utf8)!
+                print("reply: \(jsonReplyString)")
+            case .echo(let body):
+                let reply = await node.handleEcho(
+                    message: decodedMessage,
+                    body: body
+                )
+                let jsonReply = try jsonEncoder.encode(reply)
+                let jsonReplyString = String(data: jsonReply, encoding: .utf8)!
+                print("reply: \(jsonReplyString)")
+            }
+            print("~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~")
         }
         
-//        while let input = readLine(strippingNewline: true) {
-//            let inputData = input.data(using: .utf8)!
-//            let decodedInitMessage = try? JSONDecoder().decode(Message<InitMessageBody>.self, from: inputData)
-//            if decodedInitMessage == nil {
-//                
-//            }
-//        }
-        
-        let initMessageJSON = initMessage.data(using: .utf8)!
-        let decodedInitMessage = try! JSONDecoder().decode(Message<InitMessageBody>.self, from: initMessageJSON)
-        print(decodedInitMessage)
     }
 }
