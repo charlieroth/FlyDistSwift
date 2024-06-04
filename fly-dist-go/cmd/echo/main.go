ce30e29fcbee8944b409634cea83554a26d22916
charlieroth <charlieroth4@gmail.com>
2024-06-04 19:45:02 +0200

    go: update echo and unique-ids to use maelstrom module

--- fly-dist-go/cmd/echo/main.go
+++ fly-dist-go/cmd/echo/main.go
@@ -5,45 +5,23 @@ import (
 	"fmt"
 	"log"
 
-	m "github.com/charlieroth/gossip-gloomers/fly-dist-go/maelstrom"
+	m "github.com/jepsen-io/maelstrom/demo/go"
 )
-
-type Server struct {
-	n *m.Node
-}
-
-func (s *Server) initHandler(msg m.Message) error {
-	var body m.InitMessageBody
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return fmt.Errorf("unmarshal init message body: %w", err)
-	}
-
-	s.n.Init(body.NodeId, body.NodeIds)
-	log.Printf("Node %s initialized", s.n.Id())
-
-	return s.n.Reply(msg, map[string]any{
-		"type": "init_ok",
-	})
-}
-
-func (s *Server) echoHandler(msg m.Message) error {
-	var body m.EchoMessageBody
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return fmt.Errorf("unmarshal init message body: %w", err)
-	}
-
-	return s.n.Reply(msg, map[string]any{
-		"type": "echo_ok",
-		"echo": body.Echo,
-	})
-}
 
 func main() {
 	n := m.NewNode()
-	s := &Server{n: n}
 
-	n.Handle("init", s.initHandler)
-	n.Handle("echo", s.echoHandler)
+	n.Handle("echo", func(msg m.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return fmt.Errorf("unmarshal init message body: %w", err)
+		}
+
+		return n.Reply(msg, map[string]any{
+			"type": "echo_ok",
+			"echo": body["echo"],
+		})
+	})
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
--- fly-dist-go/cmd/unique-ids/main.go
+++ fly-dist-go/cmd/unique-ids/main.go
@@ -2,57 +2,32 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"log"
 	"math"
 	"math/rand"
 	"time"
 
-	m "github.com/charlieroth/gossip-gloomers/fly-dist-go/maelstrom"
+	m "github.com/jepsen-io/maelstrom/demo/go"
 )
-
-type Server struct {
-	n *m.Node
-}
-
-func (s *Server) initHandler(msg m.Message) error {
-	var body m.InitMessageBody
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return fmt.Errorf("unmarshal init message body: %w", err)
-	}
-
-	s.n.Init(body.NodeId, body.NodeIds)
-	log.Printf("Node %s initialized", s.n.Id())
-
-	return s.n.Reply(msg, map[string]any{
-		"type": "init_ok",
-	})
-}
-
-func (s *Server) generateHandler(msg m.Message) error {
-	var body m.MessageBody
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return err
-	}
-
-	rand.New(rand.NewSource(time.Now().UnixNano()))
-	randomId := rand.Intn(math.MaxInt)
-
-	return s.n.Reply(msg, map[string]any{
-		"type": "generate_ok",
-		"id": randomId,
-	})
-}
 
 func main() {
 	n := m.NewNode()
-	s := &Server{ n: n}
+	n.Handle("generate", func(msg m.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return err
+		}
 
-	n.Handle("init", s.initHandler)
-	n.Handle("generate", s.generateHandler)
+		rand.New(rand.NewSource(time.Now().UnixNano()))
+		randomId := rand.Intn(math.MaxInt)
+
+		return n.Reply(msg, map[string]any{
+			"type": "generate_ok",
+			"id":   randomId,
+		})
+	})
 
 	if err := n.Run(); err != nil {
 		log.Fatal(err)
 	}
 }
-
