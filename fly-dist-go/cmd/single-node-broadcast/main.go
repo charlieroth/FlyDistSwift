16a6e5ef2ee711d20af57a9a7bc0780e5c77d7e5
charlieroth <charlieroth4@gmail.com>
2024-06-04 19:40:07 +0200

    go: update single/multi-node-broadcast implementations to use fault-tolerant-broadcast implementation

--- fly-dist-go/cmd/single-node-broadcast/main.go
+++ fly-dist-go/cmd/single-node-broadcast/main.go
@@ -4,98 +4,66 @@ import (
 	"encoding/json"
 	"fmt"
 	"log"
-	"sync"
+	"time"
 
-	m "github.com/charlieroth/gossip-gloomers/fly-dist-go/maelstrom"
+	"github.com/charlieroth/gossip-gloomers/fly-dist-go/internal"
+	m "github.com/jepsen-io/maelstrom/demo/go"
 )
-
-type Server struct {
-	n *m.Node
-	messagesMu sync.RWMutex
-	messages  map[int]int8
-}
-
-func (s *Server) allMessages() []int {
-	s.messagesMu.RLock()
-	messages := make([]int, 0, len(s.messages))
-	for message := range s.messages {
-		messages = append(messages, message)
-	}
-	s.messagesMu.RUnlock()
-
-	return messages
-}
-
-func (s *Server) initHandler(msg m.Message) error {
-	var body m.InitMessageBody
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return fmt.Errorf("unmarshal init message body: %w", err)
-	}
-
-	s.n.Init(body.NodeId, body.NodeIds)
-	log.Printf("Node %s initialized", s.n.Id())
-
-	return s.n.Reply(msg, map[string]any{
-		"type": "init_ok",
-	})
-}
-
-func (s *Server) topologyHandler(msg m.Message) error {
-	var body m.TopologyMessageBody
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return fmt.Errorf("unmarshal init message body: %w", err)
-	}
-
-	return s.n.Reply(msg, map[string]any{
-		"type": "topology_ok",
-	})
-}
-
-func (s *Server) broadcastHandler(msg m.Message) error {
-	var body m.BroadcastMessageBody
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return fmt.Errorf("unmarshal init message body: %w", err)
-	}
-	
-	s.messagesMu.Lock()
-	if _, exists := s.messages[body.Message]; exists {
-		s.messagesMu.Unlock()
-		return nil
-	}
-	s.messages[body.Message] = 1
-	s.messagesMu.Unlock()
-
-	return s.n.Reply(msg, map[string]any{
-		"type": "broadcast_ok",
-	})
-}
-
-func (s *Server) readHandler(msg m.Message) error {
-	var body m.MessageBody
-	if err := json.Unmarshal(msg.Body, &body); err != nil {
-		return fmt.Errorf("unmarshal init message body: %w", err)
-	}
-
-	messages := s.allMessages()
-
-	return s.n.Reply(msg, map[string]any{
-		"type": "read_ok",
-		"messages": messages,
-	})
-}
 
 func main() {
 	n := m.NewNode()
-	s := &Server{n: n, messages: make(map[int]int8)}
+	store := internal.NewStore()
+	gossiper := internal.NewGossiper(100 * time.Millisecond)
 
-	n.Handle("init", s.initHandler)
-	n.Handle("topology", s.topologyHandler)
-	n.Handle("broadcast", s.broadcastHandler)
-	n.Handle("read", s.readHandler)
+	n.Handle("topology", func(msg m.Message) error {
+		gossiper.Start(store, n, 5)
+		return n.Reply(msg, map[string]any{
+			"type": "topology_ok",
+		})
+	})
+
+	n.Handle("gossip", func(msg m.Message) error {
+		var body internal.GossipMessage
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return fmt.Errorf("unmarshal init message body: %w", err)
+		}
+
+		for _, message := range body.Messages {
+			store.Add(message)
+		}
+
+		return n.Reply(msg, body)
+	})
+
+	n.Handle("broadcast", func(msg m.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return fmt.Errorf("unmarshal init message body: %w", err)
+		}
+
+		bodyMsg := body["message"]
+		v := bodyMsg.(float64)
+		store.Add(v)
+
+		return n.Reply(msg, map[string]any{
+			"type": "broadcast_ok",
+		})
+	})
+
+	n.Handle("read", func(msg m.Message) error {
+		var body map[string]any
+		if err := json.Unmarshal(msg.Body, &body); err != nil {
+			return fmt.Errorf("unmarshal init message body: %w", err)
+		}
+
+		return n.Reply(msg, map[string]any{
+			"type":     "read_ok",
+			"messages": store.ReadAll(),
+		})
+	})
 
 	if err := n.Run(); err != nil {
+		gossiper.Stop()
 		log.Fatal(err)
 	}
 }
-
-
