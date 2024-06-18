903d6c928fd1b51391fd73bcf534e08bc3cecbfd
charlieroth <charlieroth4@gmail.com>
2024-06-18 17:56:48 +0200

    swift: add files to FaultTolerantBroadcast project

---
+++ swift/FaultTolerantBroadcast/Sources/FaultTolerantBroadcast.swift
@@ -0,0 +1,55 @@
+//
+//  FaultTolerantBroadcast.swift
+//
+//
+//  Created by Charles Roth on 2024-06-08.
+//
+
+import Foundation
+
+@main
+struct FaultTolerantBroadcast {
+    static func main() async throws {
+        let stderr = StandardError()
+        let node = Node()
+        while let line = readLine(strippingNewline: true) {
+            let data = line.data(using: .utf8)!
+            let decoder = JSONDecoder()
+            let req = try decoder.decode(MaelstromMessage.self, from: data)
+            
+            switch req.body {
+            case .initMessage(let body):
+                Task {
+                    try await node.handleInit(req: req, body: body)
+                }
+                break
+            case .topologyMessage(let body):
+                Task {
+                    try await node.handleTopology(req: req, body: body)
+                }
+                break
+            case .broadcastMessage(let body):
+                Task {
+                    try await node.handleBroadcast(req: req, body: body)
+                }
+                break
+            case .broadcastOkMessage(let body):
+                if let msgId = body.in_reply_to {
+                    if let rpcNode = await node.callbacks[msgId] {
+                            await rpcNode.received(msg: body)
+                            await node.removeCallback(msgId: msgId)
+                    }
+                }
+                break
+            case .readMessage(let body):
+                Task {
+                    try await node.handleRead(req: req, body: body)
+                }
+                break
+            default:
+                stderr.write("no message handler for type: \(req.body.type)\n")
+                break
+            }
+        }
+    }
+}
