6138365574d5f317bb8373b3afe3b22d70a20e65
charlieroth <charlieroth4@gmail.com>
2024-06-23 17:13:39 +0200

    Sandbox 2024.06.23..17.13

--- Sandbox.playground/Contents.swift
+++ Sandbox.playground/Contents.swift
@@ -1,12 +1,36 @@
 import Foundation
 
-var tasks: [Int:Bool] = [:]
+enum SandboxError: Error {
+    case failedToAssignKeyToNode
+}
 
-tasks[1] = false
-tasks[2] = true
+var id = "n0"
+var nodeIds = ["n0", "n1"]
 
-print(tasks.count)
+func keyToNode(key: String) throws -> String {
+    if let keyInt = Int(key) {
+        let nodeId = keyInt % nodeIds.count
+        return "n\(nodeId)"
+    }
+    
+    throw SandboxError.failedToAssignKeyToNode
+}
 
-tasks.removeValue(forKey: 1)
+func keysForLocalAndRemote(keys: [String]) throws -> ([String], [String]) {
+    let forSelf = try keys.filter { key in
+        let nodeId = try keyToNode(key: key)
+        return nodeId == id
+    }
+    
+    let forRemote = try keys.filter { key in
+        let nodeId = try keyToNode(key: key)
+        return nodeId != id
+    }
+    
+    return (forSelf, forRemote)
+}
 
-print(tasks.count)
+let node = try keyToNode(key: "5")
+let (localKeys, remoteKeys) = try keysForLocalAndRemote(keys: ["5"])
+localKeys
+remoteKeys
